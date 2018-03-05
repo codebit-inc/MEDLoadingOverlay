147838cfcf3361b6671f0d6f4480cddb618d0433
Kaixin Lian <kaixin.lian@gmail.com>
2018-03-05 23:21:22 +0800

    [add] Implementing MEDLoadingOverlay.

---
+++ MEDLoadingOverlay/LoadingOverlay/MEDActivityIndicatorView.swift
@@ -0,0 +1,155 @@
+//
+//  MEDActivityIndicatorView.swift
+//  MEDLoadingOverlay
+//
+//  Created by Eli Lien on 3/5/18.
+//  Copyright © 2018 CODEBIT INC. All rights reserved.
+//
+
+import UIKit
+
+private let kMEDSpinningAnimationKey = "Rotation"
+
+@IBDesignable public class MEDActivityIndicatorView: UIView {
+
+    @IBInspectable public var lineWidth: CGFloat = 2.0 {
+        didSet {
+            self.circlePathLayer.lineWidth = lineWidth
+        }
+    }
+    private(set) public var isAnimating = false
+    @IBInspectable public var autoStartAnimating: Bool = false {
+        didSet {
+            if autoStartAnimating && self.superview != nil {
+                self.animate(animated: true)
+            }
+        }
+    }
+    @IBInspectable public var hidesWhenStopped: Bool = false {
+        didSet {
+            
+        }
+    }
+    @IBInspectable public var color: UIColor = UIColor.lightGray {
+        didSet {
+            self.circlePathLayer.strokeColor = color.cgColor
+        }
+    }
+    
+    private var circlePathLayer = CAShapeLayer()
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configure()
+    }
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        self.circlePathLayer.frame = bounds
+        self.circlePathLayer.path = self.circlePath().cgPath
+    }
+    
+    override public func willMove(toSuperview newSuperview: UIView?) {
+        super.willMove(toSuperview: newSuperview)
+        
+        if newSuperview != nil {
+            if self.autoStartAnimating {
+                self.animate(animated: true)
+            }
+        } else {
+            self.animate(animated: false)
+        }
+    }
+    
+    
+    public func startAnimating() {
+        self.animate(animated: true)
+    }
+    
+    public func stopAnimating() {
+        self.animate(animated: false)
+    }
+    
+    private func animate(animated: Bool) {
+        if animated {
+            self.isHidden = false
+            
+            if self.isAnimating == false {
+                // create or resume
+                if self.circlePathLayer.animation(forKey: kMEDSpinningAnimationKey) == nil {
+                    self.createAnimationLayer(layer: self.circlePathLayer)
+                } else {
+                    self.resumeLayer(layer: self.circlePathLayer)
+                }
+            }
+            
+            self.isAnimating = true
+        } else {
+            self.isAnimating = false
+            self.pauseLayer(layer: self.circlePathLayer)
+            if self.hidesWhenStopped {
+                self.isHidden = true
+            }
+        }
+    }
+    
+    private func pauseLayer(layer: CALayer) {
+        let pausedTime = layer.convertTime(CACurrentMediaTime(), from: nil)
+        layer.speed = 0.0
+        layer.timeOffset = pausedTime
+    }
+    
+    private func resumeLayer(layer: CALayer) {
+        let pausedTime = layer.timeOffset
+        layer.speed = 1.0
+        layer.timeOffset = 0.0
+        layer.beginTime = 0.0
+        let timeSincePaused = layer.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
+        layer.beginTime = timeSincePaused
+    }
+    
+    private func createAnimationLayer(layer: CALayer) {
+        let animation = CABasicAnimation(keyPath: "transform.rotation.z")
+        animation.fromValue = NSNumber(value: 0.0)
+        animation.toValue = NSNumber(value: 2.0 * Double.pi)
+        animation.duration = 0.9;
+        animation.isRemovedOnCompletion = false // prevent from getting remove when app enter background or view disappear
+        animation.repeatCount = Float.infinity
+        layer.add(animation, forKey: kMEDSpinningAnimationKey)
+    }
+    
+    private func circleFrame() -> CGRect {
+        // Align center
+        let diameter = min(self.circlePathLayer.bounds.size.width, self.circlePathLayer.bounds.size.height)
+        var circleFrame = CGRect(x: 0.0, y: 0.0, width: diameter, height: diameter)
+        circleFrame.origin.x = circlePathLayer.bounds.midX - circleFrame.midX
+        circleFrame.origin.y = circlePathLayer.bounds.midY - circleFrame.midY
+        
+        // Offset lineWidth
+        let inset = self.circlePathLayer.lineWidth / 2.0
+        circleFrame = circleFrame.insetBy(dx: inset, dy: inset)
+        
+        return circleFrame
+        
+    }
+    
+    private func circlePath() -> UIBezierPath {
+        return UIBezierPath(ovalIn: self.circleFrame())
+    }
+    
+    private func configure() {
+        circlePathLayer.frame = bounds
+        circlePathLayer.lineWidth = self.lineWidth
+        circlePathLayer.fillColor = UIColor.clear.cgColor
+        circlePathLayer.strokeColor = self.color.cgColor
+        circlePathLayer.strokeEnd = 0.9
+        layer.addSublayer(circlePathLayer)
+        backgroundColor = UIColor.white
+    }
+
+}
---
+++ MEDLoadingOverlay/LoadingOverlay/MEDLoadingOverlay.swift
@@ -0,0 +1,48 @@
+//
+//  MEDLoadingOverlay.swift
+//  MEDLoadingOverlay
+//
+//  Created by Eli Lien on 3/5/18.
+//  Copyright © 2018 CODEBIT INC. All rights reserved.
+//
+
+import UIKit
+
+class MEDLoadingOverlay: UIView {
+
+    static let shared = MEDLoadingOverlay()
+    
+    var overlayView = UIView()
+    var activityIndicatorView = MEDActivityIndicatorView()
+    var activityIndicatorBackView = UIVisualEffectView()
+    
+    private var circlePathLayer = CAShapeLayer()
+    
+    func showOverlay(view: UIView!) {
+        self.overlayView = UIView(frame: UIScreen.main.bounds)
+        self.overlayView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5)
+        let blurEffect = UIBlurEffect(style: .extraLight)
+        self.activityIndicatorBackView = UIVisualEffectView(effect: blurEffect)
+        self.activityIndicatorBackView.frame = CGRect(x: 0.0, y: 0.0, width: 60.0, height: 60.0)
+        self.activityIndicatorBackView.layer.cornerRadius = 5.0
+        self.activityIndicatorBackView.clipsToBounds = true
+        self.activityIndicatorBackView.center = self.overlayView.center
+        self.activityIndicatorBackView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.overlayView.addSubview(self.activityIndicatorBackView)
+        
+        self.activityIndicatorView.frame = CGRect(x: 0.0, y: 0.0, width: 40.0, height: 40.0)
+        self.activityIndicatorView.lineWidth = 1.0
+        self.activityIndicatorView.color = UIColor(hex: "#555765")
+        self.activityIndicatorView.backgroundColor = UIColor.clear
+        self.activityIndicatorView.center = self.overlayView.center
+        self.activityIndicatorView.startAnimating()
+        self.overlayView.addSubview(self.activityIndicatorView)
+        view.addSubview(self.overlayView)
+    }
+    
+    func hideOverlayView() {
+        self.activityIndicatorView.stopAnimating()
+        self.overlayView.removeFromSuperview()
+    }
+
+}
